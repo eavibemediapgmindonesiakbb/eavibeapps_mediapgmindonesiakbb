c9544ac1d5034218a25d23b37b16cea9889bd1d4
eavibemediapgmindonesiakbb <pgmindonesiakbb23@gmail.com>
2026-05-26 21:37:45 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'pgm-kbb-v1'; // Ganti v1 jadi v2, v3 tiap update
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Langsung aktif tanpa nunggu
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key)) // Hapus cache lama
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
